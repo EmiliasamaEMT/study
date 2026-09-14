e19df698108994fbdb0f97abd81ea832cc070e22
EmiliasamaEMT <yangxinyu611@163.com>
2026-09-14 20:34:33 +0800

    1

--- p2292.cpp
+++ p2292.cpp
@@ -3,12 +3,14 @@
 #include <queue>
 using namespace std;
 
+
 int main()
 {
     int n,m,k;
+    int ans=0;
     cin>>n>>m>>k;
     vector<vector<int>> graph(1001);
-    for(int i = 1; i <= n; i++)
+    for(int i = 0; i < n; i++)
     {
         int u,v;
         cin>>u>>v;
@@ -16,4 +18,26 @@ int main()
         graph[v].push_back(u);
 
     }
+
+    vector<int> dis(1001,-1);
+    queue<int> q;
+    q.push(m);
+    dis[m]=0;
+    while (q.size()!=0)
+    {
+        int x=q.front();
+        q.pop();
+
+        if (dis[x]==k)
+            continue;
+        for (int y:graph[x])
+        {
+            if (dis[y]!=-1)
+                continue;
+            dis[y]=dis[x]+1;
+            ans++;
+            q.push(y);
+        }
+    }
+    cout<<ans;
 }
